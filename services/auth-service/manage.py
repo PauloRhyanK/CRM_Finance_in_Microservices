6e72281441ed73b9e5ad962ec10bea4afee5e174
Paulo Rhyan Kuster <103347114+PauloRhyanK@users.noreply.github.com>
2025-10-22 01:39:59 +0000

    fix: manage.py

---
+++ services/auth-service/manage.py
@@ -0,0 +1,10 @@
+# /auth-service/manage.py
+import os
+from app import create_app, db
+from config import config_by_name
+from flask_migrate import Migrate
+
+config_name = os.getenv('FLASK_ENV', 'default')
+config_object = config_by_name[config_name]
+app = create_app(config_object)
+migrate = Migrate(app, db)
